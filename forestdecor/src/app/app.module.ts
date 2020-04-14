2545fc82cd51bc82b17231b0332ff41c106ffae9
SergeySalimov <salimov_s@tut.by>
2020-04-14 14:56:50 +0300

    feat(all project): init project, adding bootstrap style

--- forestdecor/src/app/app.module.ts
+++ forestdecor/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LinkStringComponent } from './header/link-string/link-string.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LinkStringComponent
   ],
   imports: [
     BrowserModule,
