d141b41cfa5abc5dd906962ca3be2b3c6d0bbedb
SergeySalimov <salimov_s@tut.by>
2020-04-14 15:10:42 +0300

    feat(all project): start point

--- forestdecor/src/app/app.module.ts
+++ forestdecor/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { LinkStringComponent } from './header/link-string/link-string.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    LinkStringComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
