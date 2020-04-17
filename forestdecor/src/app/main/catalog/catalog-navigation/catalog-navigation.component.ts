cafb80ffbb29282bec37534b8b1e17e1a1b7acfe
SergeySalimov <salimov_s@tut.by>
2020-04-18 00:04:55 +0300

    feat(components): added catalog/catalog-navigation

--- forestdecor/src/app/main/catalog/catalog-navigation/catalog-navigation.component.ts
+++ forestdecor/src/app/main/catalog/catalog-navigation/catalog-navigation.component.ts
@@ -1,15 +1,76 @@
 import { Component, OnInit } from '@angular/core';
+import { FlatTreeControl } from "@angular/cdk/tree";
+import { MatTreeFlatDataSource, MatTreeFlattener } from "@angular/material/tree";
+
+/**
+ * Food data with nested structure.
+ * Each node has a name and an optional list of children.
+ */
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Fruit',
+    children: [
+      {
+        name: 'Apple',
+        children: [
+          {name: 'Green'}
+        ],
+      },
+      {name: 'Banana'},
+      {name: 'Fruit loops'},
+    ]
+  }, {
+    name: 'Vegetables'
+  },
+];
+
+/** Flat node with expandable and level information */
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
+
+/**
+ * @title Tree with flat nodes
+ */
 
 @Component({
   selector: 'app-catalog-navigation',
   templateUrl: './catalog-navigation.component.html',
   styleUrls: ['./catalog-navigation.component.scss']
 })
+
 export class CatalogNavigationComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
 
   ngOnInit(): void {
   }
+
+  private _transformer = (node: FoodNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level: level,
+    };
+  }
+
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    node => node.level, node => node.expandable);
+
+  treeFlattener = new MatTreeFlattener(
+    this._transformer, node => node.level, node => node.expandable, node => node.children);
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
 
 }
