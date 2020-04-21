0845dd1717aeec2dcaaa54373e4c6e5612c7c157
SergeySalimov <salimov_s@tut.by>
2020-04-21 21:31:01 +0300

    feat(project): add services Auth & Products

---
+++ forestdecor/src/app/shared/services/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private _isLogged = false;
+
+  constructor(private router: Router) { }
+
+  get isLogged() {
+    return this._isLogged;
+  }
+
+  login() {
+    this._isLogged = true;
+    this.router.navigate(['/messages']);
+    console.log('login');
+  }
+
+  logout() {
+    this._isLogged = false;
+    this.router.navigate(['']);
+    console.log('logout');
+  }
+
+}
--- forestdecor/src/app/shared/setHeight/set-height.directive.ts
+++ forestdecor/src/app/shared/setHeight/set-height.directive.ts
@@ -8,22 +8,27 @@ export class SetHeightDirective implements OnInit, AfterViewInit {
   constructor(public element: ElementRef, public renderer: Renderer2) { }
 
   // @HostListener('click', ['$event']) onMouseEnter(event: MouseEvent): void {
-    // console.dir((event.target as HTMLElement).innerText);
-    // console.log('in');
+  //   console.dir((event.target as HTMLElement).innerText);
+  //   console.log('in');
   // }
 
-  @HostBinding('style.height') get height() {
-    return '700px';
-  }
+  // @HostBinding('style.height') get height() {
+  //   console.log((this.element.nativeElement as HTMLElement).clientHeight);
+  //   return '700px';
+  // }
 
   ngOnInit(): void {
     // console.log((this.element.nativeElement as HTMLElement).clientHeight);
     // const elHeight = (this.element.nativeElement as HTMLElement).clientHeight;
-    // this.renderer.setStyle(this.element.nativeElement, 'height', elHeight);
-    // this.renderer.setStyle(this.element.nativeElement, 'height', `${elHeight * 2}px`);
+    // this.renderer.setStyle(this.element.nativeElement, 'height', 700);
+    // this.renderer.setStyle(this.element.nativeElement, 'height', `${elHeight + 100}px`);
   }
 
   ngAfterViewInit(): void {
+    // console.log((this.element.nativeElement as HTMLElement).clientHeight);
+    const elHeight = (this.element.nativeElement as HTMLElement).clientHeight;
+    // this.renderer.setStyle(this.element.nativeElement, 'height', 700);
+    this.renderer.setStyle(this.element.nativeElement, 'height', `${elHeight * 2}px`);
     console.log((this.element.nativeElement as HTMLElement).clientHeight);
   }
 
