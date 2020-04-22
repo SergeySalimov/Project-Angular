c310eed82ae5cf915075b34c72146ad4d0a8b03e
SergeySalimov <salimov_s@tut.by>
2020-04-22 23:20:51 +0300

    feat(component): add catalog-products

---
+++ forestdecor/src/app/main/catalog/catalog-products/catalog-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-products',
+  templateUrl: './catalog-products.component.html',
+  styleUrls: ['./catalog-products.component.scss']
+})
+export class CatalogProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
