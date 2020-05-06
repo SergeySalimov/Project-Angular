04b4482e31693339cd932c3e0091501b92bbac7c
SergeySalimov <salimov_s@tut.by>
2020-05-06 22:26:07 +0300

    feat(pipes): added pipes phoneBY

---
+++ forestdecor/src/app/shared/pipes/phone-by.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PhoneBYPipe } from './phone-by.pipe';
+
+describe('PhoneBYPipe', () => {
+  it('create an instance', () => {
+    const pipe = new PhoneBYPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
