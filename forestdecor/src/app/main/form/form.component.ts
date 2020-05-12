92503fb813fdf0265c170478709eda8598f3512c
SergeySalimov <salimov_s@tut.by>
2020-05-12 16:53:41 +0300

    feat(registration form): did TDform with valid/invalid hints+ 2psw check

--- forestdecor/src/app/main/form/form.component.ts
+++ forestdecor/src/app/main/form/form.component.ts
@@ -1,35 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, NavigationStart, Router } from '@angular/router';
-import { distinctUntilChanged, filter, map, tap } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit {
-
-  isRegistration = false;
-
-  constructor( private router: Router) {
-  }
-
-  //ToDo need to check it out
-  ngOnInit(): void {
-    this.router.events.pipe(
-      filter( event => event instanceof NavigationStart),
-      tap(e => console.dir(e)),
-      map( event => (event as NavigationEnd).url),
-      map ( strUrl => strUrl.split('/')),
-      map (urlArr => urlArr[1]),
-      distinctUntilChanged(),
-    ).subscribe(str => {
-      // console.log(str);
-    })
-  }
-
-  onFormChange(reg) {
-    this.isRegistration = reg;
-  }
-
+export class FormComponent {
+  constructor( ) { }
 }
