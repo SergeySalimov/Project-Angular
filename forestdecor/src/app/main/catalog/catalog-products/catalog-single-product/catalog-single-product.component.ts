851c5f66ff4e00c6569b69c71e123582425bd232
SergeySalimov <salimov_s@tut.by>
2020-05-15 18:14:16 +0300

    feat(single-product-component): added (back, up, all) btns

--- forestdecor/src/app/main/catalog/catalog-products/catalog-single-product/catalog-single-product.component.ts
+++ forestdecor/src/app/main/catalog/catalog-products/catalog-single-product/catalog-single-product.component.ts
@@ -13,20 +13,24 @@ import { Router } from '@angular/router';
 export class CatalogSingleProductComponent implements OnInit {
 
   @Input() curProduct: Product;
+  @Input() previous: string;
 
   constructor(
     public catalogNavigation: CatalogNavigationService,
     private productsService: ProductsService,
-    private router: Router
-  ) { }
-
-  ngOnInit(): void {
+    private router: Router,
+  ) {
   }
 
-  goBack(urlName: string) {
-    const prd: ProductPlacer = this.productsService.getProductUrlInfo(urlName);
-    let parents = prd.parents.length > 0 ? prd.parents.pop() : 'all';
-    this.router.navigate(['/catalog', parents]);
+  ngOnInit(): void {}
+
+  goBack(urlName: string, noPrev = false) {
+    if (!this.previous || noPrev) {
+      // if no previous go to parent
+      const prd: ProductPlacer = this.productsService.getProductUrlInfo(urlName);
+      this.previous = prd.parents.length > 0 ? prd.parents.pop() : 'all';
+    }
+    this.router.navigate(['/catalog', this.previous]);
   }
 
   needPhotos(prodPhoto: string[]) {
