2096cc8f9ca1a2e7c4371b177e0a06f9062d1a3f
SergeySalimov <salimov_s@tut.by>
2020-05-18 15:25:43 +0300

    feat(contact-us): added reactive form with all logic & styles

--- forestdecor/src/app/contact-us/contact-us.component.ts
+++ forestdecor/src/app/contact-us/contact-us.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { distinctUntilChanged, filter, map, tap } from 'rxjs/operators';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -10,34 +10,68 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class ContactUsComponent implements OnInit {
 
-  phoneNumber = '';
   curRoute: string;
-  email = new FormControl('', [Validators.required, Validators.email]);
 
-  constructor(private router: Router) {  }
+  formContactUs: FormGroup;
+
+  constructor(private router: Router) {
+  }
 
   ngOnInit(): void {
-   this.router.events.pipe(
-      filter( event => event instanceof NavigationEnd),
-      map( event => (event as NavigationEnd).urlAfterRedirects),
+    this._initform();
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(event => (event as NavigationEnd).urlAfterRedirects),
       distinctUntilChanged(),
-      map ( strUrl => strUrl.split('/')[1]),
+      map(strUrl => strUrl.split('/')[1]),
     )
       .subscribe((route: string) => {
         this.curRoute = route;
       })
   }
 
-  onChangePhone(event): void {
-    this.phoneNumber = event;
+  private _initform() {
+    this.formContactUs = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      email: new FormControl('', Validators.email),
+      phone: new FormControl(''),
+      message: new FormControl('', [Validators.required, Validators.minLength(10)]),
+      isRegisterAfter: new FormControl(false),
+    });
+  }
+
+  get name() {
+    return this.formContactUs.get('name');
+  }
+
+  get email() {
+    return this.formContactUs.get('email');
+  }
+
+  get phone() {
+    let phone = this.formContactUs.get('phone');
+    if (phone.value === '(') this.formContactUs.patchValue({phone: ''});
+    return phone;
+  }
+
+  get message() {
+    return this.formContactUs.get('message');
+  }
+
+  onSubmit() {
+    console.log(this.formContactUs);
+  }
+
+  onClick() {
+    return false;
   }
 
   getErrorMessage() {
     if (this.email.hasError('required')) {
-      return 'You must enter a value';
+      return 'Введите email или телефон';
     }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+    return this.email.hasError('email') ? 'Не правильный email' : '';
   }
 
 }
