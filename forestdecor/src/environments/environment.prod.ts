da9bc188e8eb6813c9a650d00706ec5ec59919b9
SergeySalimov <salimov_s@tut.by>
2020-05-28 16:18:44 +0300

    feat(project): preparation for deploy

--- forestdecor/src/environments/environment.prod.ts
+++ forestdecor/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDIGANkFkG2s7bszuIFcyJrByruYVSsYPg",
+    authDomain: "prj-forestdecor.firebaseapp.com",
+    databaseURL: "https://prj-forestdecor.firebaseio.com",
+    projectId: "prj-forestdecor",
+    storageBucket: "prj-forestdecor.appspot.com",
+    messagingSenderId: "658481150043",
+    appId: "1:658481150043:web:d64c695bc14a7e494ce77e",
+    measurementId: "G-HW8Y1LCCQ0"
+  }
 };
