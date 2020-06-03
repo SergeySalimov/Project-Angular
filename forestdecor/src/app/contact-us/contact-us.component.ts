5cd0d15ac8b39df82a1c82c3787324254fea844b
SergeySalimov <salimov_s@tut.by>
2020-06-03 12:38:53 +0300

    feat(project): added pathData to form after userLogin, hide on adminMode

--- forestdecor/src/app/contact-us/contact-us.component.ts
+++ forestdecor/src/app/contact-us/contact-us.component.ts
@@ -5,6 +5,8 @@ import { FormControl, FormGroup, FormGroupDirective, Validators } from '@angular
 import { ContactUsService } from './contact-us-service/contact-us.service';
 import { UserData } from '../shared/models/userData';
 import { Subscription } from 'rxjs';
+import { AuthService } from '../shared/services/auth/auth.service';
+import { User } from '../shared/services/auth/user';
 
 @Component({
   selector: 'app-contact-us',
@@ -16,50 +18,38 @@ export class ContactUsComponent implements OnInit, OnDestroy {
   curRoute: string;
   formContactUs: FormGroup;
   isRegisterAfter: boolean;
-  controlPhoneSubscription: Subscription;
+  isUserNotLog = true;
+  isUserAdmin = false;
+  phoneSubscr: Subscription;
   routerSubscription: Subscription;
-
+  auhtSubscr: Subscription;
 
   @ViewChild(FormGroupDirective) formGroupDirective: FormGroupDirective;
 
-  constructor(private router: Router, private contactUs: ContactUsService) {
+  constructor(private router: Router, private contactUs: ContactUsService, private auth: AuthService) {
   }
 
   ngOnInit(): void {
     this._initform();
-    this.controlPhoneSubscription = this.formContactUs.controls['phone'].valueChanges.pipe(
-      map(phone => phone === '(' ? '' : phone)
-    )
-      .subscribe(
-        phone => {
+
+    this.phoneSubscr = this.formContactUs.controls['phone'].valueChanges.pipe(map(ph => ph === '(' ? '' : ph))
+      .subscribe(phone => {
           phone === '' ? this.emailState.setValidators([Validators.required, Validators.email]) :
             this.emailState.setValidators([Validators.email]);
           this.emailState.updateValueAndValidity();
-        }
-      );
+        });
+
     this.routerSubscription = this.router.events.pipe(
       filter(event => event instanceof NavigationEnd),
       map(event => (event as NavigationEnd).urlAfterRedirects),
       distinctUntilChanged(),
-      map(strUrl => strUrl.split('/')[1]),
-    )
-      .subscribe((route: string) => {
-        this.curRoute = route;
-      })
-  }
+      map(strUrl => strUrl.split('/')[1]))
+      .subscribe((route: string) => this.curRoute = route);
 
-  onSubmit() {
-    this.isRegisterAfter = this.formContactUs.value.isRegisterAfter;
-    this.contactUs.sendMessage(this.formContactUs.value)
-      .subscribe(() => {
-        if (this.isRegisterAfter) {
-          const forQueryParams = this.formContactUs.value;
-          delete forQueryParams.message;
-          delete forQueryParams.isRegisterAfter;
-          this.router.navigate(['/form', 'registration'], { queryParams: (forQueryParams as UserData)})
-        }
-        this.resetForm();
-      });
+    this.auhtSubscr = this.auth.user.pipe(
+      tap(user => !!user ? [this.isUserNotLog, this.isUserAdmin]=[false, user.isAdmin] :
+        [this.isUserNotLog, this.isUserAdmin]=[true, false]))
+      .subscribe((user: User) => ((!!user && !this.isUserAdmin) || !user) ? this.patchUserData(user) : null);
   }
 
   get name() {
@@ -97,6 +87,41 @@ export class ContactUsComponent implements OnInit, OnDestroy {
     return this.emailState.hasError('email') ? 'Не правильный email' : '';
   }
 
+  onSubmit() {
+    this.isRegisterAfter = this.formContactUs.value.isRegisterAfter;
+    this.contactUs.sendMessage(this.formContactUs.value)
+      .subscribe(() => {
+        if (this.isRegisterAfter) {
+          const forQueryParams = this.formContactUs.value;
+          delete forQueryParams.message;
+          delete forQueryParams.isRegisterAfter;
+          this.router.navigate(['/form', 'registration'], { queryParams: (forQueryParams as UserData)})
+        }
+        this.resetForm();
+      });
+  }
+
+  resetForm() {
+    this.formGroupDirective.resetForm();
+  }
+
+  patchUserData(user: User) {
+    console.log('srabotalo');
+    if (!!user) {
+      const [name, email, phone] = [user.name, user.email, user.phone];
+      this.formContactUs.patchValue({name, email, phone})
+    } else {
+      this.formContactUs.reset();
+    }
+  }
+
+
+  ngOnDestroy(): void {
+    this.phoneSubscr.unsubscribe();
+    this.routerSubscription.unsubscribe();
+    this.auhtSubscr.unsubscribe();
+  }
+
   private _initform() {
     this.formContactUs = new FormGroup({
       name: new FormControl('', [Validators.required, Validators.minLength(2)]),
@@ -106,14 +131,4 @@ export class ContactUsComponent implements OnInit, OnDestroy {
       isRegisterAfter: new FormControl(false),
     });
   }
-
-  resetForm() {
-    this.formGroupDirective.resetForm();
-  }
-
-  ngOnDestroy(): void {
-    this.controlPhoneSubscription.unsubscribe();
-    this.routerSubscription.unsubscribe();
-  }
-
 }
