83874d440d1e90dece97942d155660a3743450cc
SergeySalimov <salimov_s@tut.by>
2020-06-07 15:10:42 +0300

    feat(project): btn-go-top move to shared module

--- forestdecor/src/app/main/catalog/catalog.module.ts
+++ forestdecor/src/app/main/catalog/catalog.module.ts
@@ -4,21 +4,21 @@ import { RouterModule } from '@angular/router';
 import { CatalogComponent } from './catalog.component';
 import { SharedModule } from '../../shared/shared.module';
 import { MaterialModule } from '../../material/material-module';
-import { ButtonGoTopComponent } from './button-go-top/button-go-top.component';
 import { CatalogNavigationComponent } from './catalog-navigation/catalog-navigation.component';
 import { CatalogProductsComponent } from './catalog-products/catalog-products.component';
 import { CatalogCardDeskComponent } from './catalog-products/catalog-card-desk/catalog-card-desk.component';
 import { CatalogSingleProductComponent } from './catalog-products/catalog-single-product/catalog-single-product.component';
 import { CatalogRoutingModule } from './catalog-routing.module';
+import { ButtonShowComponent } from './catalog-products/buttons/button-show/button-show.component';
 
 @NgModule({
   declarations: [
     CatalogComponent,
-    ButtonGoTopComponent,
     CatalogNavigationComponent,
     CatalogProductsComponent,
     CatalogCardDeskComponent,
-    CatalogSingleProductComponent
+    CatalogSingleProductComponent,
+    ButtonShowComponent
   ],
   imports: [
     CommonModule,
