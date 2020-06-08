aadae1a31c39ffffd4387196d570e9bb425f8e7f
SergeySalimov <salimov_s@tut.by>
2020-06-08 14:44:41 +0300

    feat(photo-service): all server methods done

--- forestdecor/src/app/shared/services/photo/photo.service.ts
+++ forestdecor/src/app/shared/services/photo/photo.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from '@angular/fire/storage';
-import { ConsoleService } from '../console/console.service';
-import { MESSAGE } from '../console/response-messages-list';
 import { BehaviorSubject } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { map, switchMap, tap } from 'rxjs/operators';
+import { PhotoUrl } from '../../models/photo-url.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,20 +13,12 @@ export class PhotoService {
 
   private _uploadProgress: BehaviorSubject<number> = new BehaviorSubject<number>(0);
 
-  constructor(private storage: AngularFireStorage, private console: ConsoleService) {
+  constructor(private storage: AngularFireStorage, private http: HttpClient) {
   }
 
   get uploadProgress() {
     return this._uploadProgress.asObservable();
   }
-
-  showFileLog() {
-    const name = "shishki-olkhi.jpg";
-    console.log('show');
-    this.storage.ref(name).getDownloadURL().subscribe((data) => console.log(data)
-    )
-  }
-
 
   uploadFile(file, folder: string) {
     const filePath = `/${folder}/${file.name}`;
@@ -33,9 +27,47 @@ export class PhotoService {
     task.percentageChanges().subscribe(number => this._uploadProgress.next(number));
     task.then((data) => {
       console.log(data.metadata.name);
-      // this.console.showInfoMessage(MESSAGE.FILE_LOADED, MESSAGE.FILE_LOADED_TIMER);
-      setTimeout(() => {this._uploadProgress.next(0)}, 2000)
+      const filePath = `/${folder}/${data.metadata.name}`;
+      this.storage.ref(filePath).getDownloadURL().pipe(
+        tap(() => setTimeout(() => {this._uploadProgress.next(0)}, 2000)),
+        switchMap((url: string) => this.sendPhotoUrlToServer(folder, [url])),
+        )
+        .subscribe(data => {
+        console.log(data);
+      });
     });
   }
 
+  updatePhotoUrl(folder, newestData) {
+    return this.deletePhotoFromServer(folder).pipe(
+      switchMap(() => this.sendPhotoUrlToServer(folder, newestData))
+    );
+  }
+
+  //ServerBlock
+  sendPhotoUrlToServer(folder: string, data) {
+    const headers: HttpHeaders = new HttpHeaders({[environment.NEED_TOKEN]: 'Add-my-token', [environment.GLOBAL_SPINNER]: 'spinnerNeeded'});
+    return this.http.post<string[]>(`${environment.firebase.databaseURL}/photos/${folder}.json`, data, {headers});
+  }
+
+  deletePhotoFromServer(folder: string) {
+    const headers: HttpHeaders = new HttpHeaders({[environment.NEED_TOKEN]: 'Add-my-token', [environment.GLOBAL_SPINNER]: 'spinnerNeeded'});
+    return this.http.delete<string[]>(`${environment.firebase.databaseURL}/photos/${folder}.json`, {headers});
+  }
+
+  getPhotosFromServer() {
+    return this.http.get<PhotoUrl[]>(`${environment.firebase.databaseURL}/photos.json`).pipe(
+      map(data => {
+        const photoUrl: PhotoUrl[] = [];
+        for (let key in data) {
+          const [fakeCopy, urlArr] = [data[key], []];
+          for (let key2 in fakeCopy) {
+            urlArr.push(...fakeCopy[key2])
+          }
+          photoUrl.push({urlName: key, urlList: [...new Set(urlArr)]});
+        }
+        return photoUrl;
+      }),
+    )
+  }
 }
