a052cf562fd678f5d2d52abcea19d1862c339f34
SergeySalimov <salimov_s@tut.by>
2020-06-11 01:21:58 +0300

    fix(project): moved collapse burger on Observable, fix in Admin burger

--- forestdecor/src/app/main/messages/messages-navigation/messages-navigation.component.ts
+++ forestdecor/src/app/main/messages/messages-navigation/messages-navigation.component.ts
@@ -1,9 +1,9 @@
 import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { environment } from '../../../../environments/environment';
-import { map, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
-import { Categorie, MsgsService, SpinnerService } from '../../../shared';
+import { Categorie, MsgsService, NavigationService, SpinnerService } from '../../../shared';
 
 @Component({
   selector: 'app-messages-navigation',
@@ -16,11 +16,13 @@ export class MessagesNavigationComponent implements OnInit, OnDestroy, AfterView
     categorie: new FormControl(Categorie[environment.START_CATEGORIE]),
   });
   serverWork$ = this.spinner.msgsSpiner;
+  collapseBurger$ = this.navigation.collapseBurger;
+
   @ViewChild('selectAll') checkbox: ElementRef;
   messagNavSubscr: Subscription;
   selectAllSubscr: Subscription;
 
-  constructor(private msgsService: MsgsService, private spinner: SpinnerService) {
+  constructor(private msgsService: MsgsService, private spinner: SpinnerService, private navigation: NavigationService) {
   }
 
   ngOnInit(): void {
