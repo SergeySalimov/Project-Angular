4c6443c4815220e00fcfed6185b40be68f6a2498
SergeySalimov <salimov_s@tut.by>
2020-06-11 14:05:03 +0300

    fix(navigation link): del payment, beautify ui

--- forestdecor/src/app/shared/services/navigation/navigation.service.ts
+++ forestdecor/src/app/shared/services/navigation/navigation.service.ts
@@ -8,18 +8,18 @@ import { BehaviorSubject } from 'rxjs';
 export class NavigationService {
 
   private _navigationLinks: NavigationLink[] = [
-    { name: 'Каталог', routerLink: '/catalog'},
-    { name: 'Доставка', routerLink: '/delivery'},
-    { name: 'Оплата', routerLink: '/payment'},
-    { name: 'Контакты', routerLink: '/contacts'},
-    { name: 'Сообщения', routerLink: '/messages'},
-    { name: 'Форма', routerLink: '/form'}, // log links
-    { name: 'Регистрация', routerLink: '/form/registration'},
-    { name: 'Авторизация', routerLink: '/form/authorization'},
-    { name: 'Восстановление пароля', routerLink: '/form/authorization/recovery'},
-    { name: 'дом', routerLink: '/home'},
-    { name: 'Главная', routerLink: '/'},
-    { name: 'Указанная страница не найдена', routerLink: '/404'},
+    { name: 'Каталог', routerLink: '/catalog', categorie: 'common'},
+    { name: 'Доставка', routerLink: '/delivery', categorie: 'common'},
+    // { name: 'Оплата', routerLink: '/payment', categorie: 'common'},
+    { name: 'Контакты', routerLink: '/contacts', categorie: 'common'},
+    { name: 'Сообщения', routerLink: '/messages', categorie: 'admin'},
+    { name: 'Форма', routerLink: '/form', categorie: 'form'}, // log links
+    { name: 'Регистрация', routerLink: '/form/registration', categorie: 'form'},
+    { name: 'Авторизация', routerLink: '/form/authorization', categorie: 'form'},
+    { name: 'Восстановление пароля', routerLink: '/form/authorization/recovery', categorie: 'form'},
+    // { name: 'дом', routerLink: '/home', categorie: 'main'},
+    { name: 'Главная', routerLink: '/', categorie: 'main'},
+    { name: 'Указанная страница не найдена', routerLink: '/404', categorie: '404'},
   ];
 
   private _collapseBurger: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
@@ -35,7 +35,8 @@ export class NavigationService {
   }
 
   getNavigationLinks(all = false): NavigationLink[] {
-    return all ? this.allLinks.slice(0,5) : this.allLinks.slice(0,4);
+    return all ? this.allLinks.filter(item => item.categorie === 'common' || item.categorie === 'admin')
+      : this.allLinks.filter(item => item.categorie === 'common');
   }
 
   get allLinks(): NavigationLink[] {
@@ -47,7 +48,7 @@ export class NavigationService {
   }
 
   get logLinks(): NavigationLink[] {
-    return this.allLinks.slice(6,9);
+    return this.allLinks.filter(item => item.categorie === 'form');
   }
 
   get recovery(): NavigationLink {
