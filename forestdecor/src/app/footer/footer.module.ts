c96a38e06cd525c54c51b38e02f4f0e6b545a3fd
SergeySalimov <salimov_s@tut.by>
2020-06-11 16:33:52 +0300

    feat(project): add footer informatiom => ready to Deploy

--- forestdecor/src/app/footer/footer.module.ts
+++ forestdecor/src/app/footer/footer.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [FooterComponent],
   exports: [FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class FooterModule { }
